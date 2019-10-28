20cfc02662e92eda521c2a6a871ea9067ba5d7df
Sebastian Ochs <sebastian.ochs@stud.tu-darmstadt.de>
2019-10-28 08:22:13 +0100

    working on preprocessing

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bert_asag',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Sebastian Ochs',
+    author_email='',
+    description='', install_requires=[]
+)
